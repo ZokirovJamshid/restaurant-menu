f0ecd40d3bed5f2f46a98036e514c37c0eca4daa
ZokirovJamshid <zakiryayevjamshid@gmail.com>
2024-12-07 13:26:52 +0500

    Initial code2

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  target: "serverless",  // Netlify uchun serverless arxitekturasi
+};
 
 export default nextConfig;
